65e09791429235157cc705d35accac6742f165b6
Vishwajeet Swain <vswain@google.com>
2025-05-06 19:56:36 +0530

    customHook

--- src/component/About.js
+++ src/component/About.js
@@ -4,8 +4,7 @@ const About = () => {
   return (
     <div>
       <h1>This is the about us page</h1>
-      <User name={"Ashwini Bari"} location={"Jaipur"} phoneNo={"9116352179"}/>
-      <User name={"Vishu"} location={"NCT"} phoneNo={"8527163949"}/>
+      <User />
 
     </div>
   );
--- src/component/UserCLass.js
+++ src/component/UserCLass.js
@@ -5,33 +5,34 @@ class User extends React.Component {
     super(props);
 
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "dummy",
+        location: "default",
+      },
     };
   }
+
+  async componentDidMount() {
+    const data = await fetch("https:/api.github.com/users/vishuswainbot");
+    const json = await data.json();
+
+    this.setState({
+      userInfo: json,
+    });
+    console.log(json);
+  }
+
   render() {
-    const { name, location, phoneNo } = this.props;
+    const { name, location, avatar_url } = this.state.userInfo;
     const { count, count2 } = this.state;
     return (
       <div className="user">
-        <h1>Count={count}</h1>
-        <button
-          className="count"
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Count
-        </button>
-
+        <img src={avatar_url}></img>
         <h2>Name:{name}</h2>
         <h3>Location:{location}</h3>
-        <h3>Phone no:{phoneNo}</h3>
       </div>
     );
   }
 }
 
 export default User;
-
